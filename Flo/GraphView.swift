228fdb9179fab5e4654fcf926e40ba5fac6e6361
farhan hussain <farhanleo23@gmail.com>
2018-09-29 14:04:03 +0200

    first commit

---
+++ Flo/GraphView.swift
@@ -0,0 +1,21 @@
+//
+//  GraphView.swift
+//  Flo
+//
+//  Created by Hussain, Farhan on 2018. 09. 28..
+//  Copyright © 2018. Razeware. All rights reserved.
+//
+
+import UIKit
+
+class GraphView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
